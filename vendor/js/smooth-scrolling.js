e736d03b12d793ca05f0585395f3ede7f882a9e6
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-10-19 02:39:04 +0530

    added advisors in team section and updates style and layout

--- vendor/js/smooth-scrolling.js
+++ vendor/js/smooth-scrolling.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 				{
 					scrollTop: $(hash).offset().top,
 				},
-				800,
+				1500,
 				function () {
 					// Add hash (#) to URL when done scrolling (default click behavior)
 					window.location.hash = hash;
