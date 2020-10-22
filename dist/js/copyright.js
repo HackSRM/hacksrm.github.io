ab055bc4bb98d28cd17afad0d806fa30263a34ff
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-10-22 18:44:44 +0530

    Updated sponsors section

--- dist/js/copyright.js
+++ dist/js/copyright.js
@@ -4,7 +4,7 @@ let startYear = 2020;
 let currentYear = new Date().getFullYear();
 
 if(startYear === currentYear){
-	document.getElementById('copyright').innerHTML = `Copyright © ${startYear} HackSRM 3.0 | <a href="https://github.com/HackSRM/hacksrm.github.io/blob/master/LICENSE">License</a>`
+	document.getElementById('copyright').innerHTML = `Copyright © ${startYear} HackSRM 3.0 | <a href="https://github.com/HackSRM/hacksrm.github.io/blob/master/LICENSE" target="_blank">License</a>`
 } else {
-	document.getElementById('copyright').innerHTML = `Copyright © ${startYear} - ${currentYear} HackSRM 3.0 | <a href="https://github.com/HackSRM/hacksrm.github.io/blob/master/LICENSE">License</a>`
+	document.getElementById('copyright').innerHTML = `Copyright © ${startYear} - ${currentYear} HackSRM 3.0 | <a href="https://github.com/HackSRM/hacksrm.github.io/blob/master/LICENSE" target="_blank">License</a>`
 }
