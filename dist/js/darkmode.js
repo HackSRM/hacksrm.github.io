e3da6b40556255d85b6e82d54a50edf0a30770ac
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-10-24 09:56:27 +0530

    OverlayScroll plugin added

--- dist/js/darkmode.js
+++ dist/js/darkmode.js
@@ -1,25 +1,33 @@
-let darkmodeState = false;
-let toolTip = document.getElementById('tltip');
+document.addEventListener('DOMContentLoaded', function () {
+	let darkmodeState = false;
+	let toolTip = document.getElementById('tltip');
 
-document.getElementById('darkmode-toggle').addEventListener('click',function () {
-    //console.log('dark mode activated');
-    darkmodeState === false ? this.style.backgroundColor = '#333' : this.style.backgroundColor = '#fff';
-    console.log(toolTip);
+	document.getElementById('darkmode-toggle').addEventListener('click', function () {
+		//console.log('dark mode activated');
+		darkmodeState === false ? (this.style.backgroundColor = '#333') : (this.style.backgroundColor = '#fff');
 
-    if(darkmodeState === false){
-        this.style.backgroundColor = '#ffedd7';
-        toolTip.style.backgroundColor = '#ffedd7';
-        toolTip.style.color = '#000'
-        darkmodeState = true;
-    }
-    else{
-        this.style.backgroundColor = '#333';
-        toolTip.style.backgroundColor = '#333';
-        toolTip.style.color = '#f8f8f8'
-        darkmodeState = false;
-    }
+		if (darkmodeState === false) {
+			this.style.backgroundColor = '#ffedd7';
+			toolTip.style.backgroundColor = '#ffedd7';
+			toolTip.style.color = '#000';
+			OverlayScrollbars(document.querySelectorAll('body'), {
+				className: 'os-theme-round-light',
+			});
 
-    document.querySelectorAll('.dark-mode').forEach(e=>{
-        e.classList.toggle('darken')
-    })
-})
+			darkmodeState = true;
+		} else {
+			this.style.backgroundColor = '#333';
+			toolTip.style.backgroundColor = '#333';
+			toolTip.style.color = '#f8f8f8';
+			OverlayScrollbars(document.querySelectorAll('body'), {
+				className: 'os-theme-round-dark',
+			});
+
+			darkmodeState = false;
+		}
+
+		document.querySelectorAll('.dark-mode').forEach(e => {
+			e.classList.toggle('darken');
+		});
+	});
+});
