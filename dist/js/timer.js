f232ef6ed879c42c62d61838ea1f35acd0f6559a
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-11-20 13:26:56 +0530

    Updated confetti adnd timer js

--- dist/js/timer.js
+++ dist/js/timer.js
@@ -1,61 +1,55 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 20, 2020 18:00:00").getTime();
+var countDownDate = new Date('Nov 20, 2020 18:01:00').getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-    // Get today's date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var x = setInterval(function () {
+	var now = new Date().getTime();
 
-    document.getElementById('dayTens').innerHTML = places(days).tens;
-    document.getElementById('dayOnes').innerHTML = places(days).ones;
+	// Find the distance between now and the count down date
+	var distance = countDownDate - now;
 
-    document.getElementById('hourTens').innerHTML = places(hours).tens;
-    document.getElementById('hourOnes').innerHTML = places(hours).ones;
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-    document.getElementById('minTens').innerHTML = places(minutes).tens;
-    document.getElementById('minOnes').innerHTML = places(minutes).ones;
-    
-    // Output the result in an element with id="demo"
-    //console.log(days + "d " + hours + "h " + minutes + "m ")
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        console.log('Hackathon Begins');
-        hackathonBegins();
+	document.getElementById('dayTens').innerHTML = places(days).tens;
+	document.getElementById('dayOnes').innerHTML = places(days).ones;
 
-        document.getElementById('dayTens').innerHTML = '0';
-        document.getElementById('dayOnes').innerHTML = '0';
+	document.getElementById('hourTens').innerHTML = places(hours).tens;
+	document.getElementById('hourOnes').innerHTML = places(hours).ones;
 
-        document.getElementById('hourTens').innerHTML = '0';
-        document.getElementById('hourOnes').innerHTML = '0';
+	document.getElementById('minTens').innerHTML = places(minutes).tens;
+	document.getElementById('minOnes').innerHTML = places(minutes).ones;
 
-        document.getElementById('minTens').innerHTML = '0';
-        document.getElementById('minOnes').innerHTML = '0';
-    }
+	if (places(hours).ones == 0 && places(minutes).ones == 0) {
+		clearInterval(x);
+		console.log('Hackathon Begins');
+		hackathonBegins();
+
+		document.getElementById('dayTens').innerHTML = '0';
+		document.getElementById('dayOnes').innerHTML = '0';
+
+		document.getElementById('hourTens').innerHTML = '0';
+		document.getElementById('hourOnes').innerHTML = '0';
+
+		document.getElementById('minTens').innerHTML = '0';
+		document.getElementById('minOnes').innerHTML = '0';
+	}
 }, 1000);
 
 let rando = 24;
 
-function places(num){
-    let ones = num%10;
-    let tens = (num-ones)/10;
+function places(num) {
+	let ones = num % 10;
+	let tens = (num - ones) / 10;
 
-    return {
-        ones: ones,
-        tens: tens
-    }
+	return {
+		ones: ones,
+		tens: tens,
+	};
 }
 
-function hackathonBegins(){
-    document.getElementById('startsIn').innerHTML = 'Hackathon has started 🎉🎊';
+function hackathonBegins() {
+	document.getElementById('startsIn').innerHTML = 'Hackathon has started 🎉🎊';
+	confetti.start(20000);
 }
